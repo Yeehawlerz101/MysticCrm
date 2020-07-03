11c31406dc1c0ce021a7aa479bbc1080f76574ca
Neil Master <yeehawlerz101@gmail.com>
2020-07-03 14:46:24 -0700

    404 Page
    Basic Html + CSS 404

---
+++ 404.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+<link rel="stylesheet" href="404.php.css">
+    <title>Mystic CRM: 404</title>
+</head>
+<body>
+<br>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+        </div>
+        <div class="col">
+
+
+            <div class="card" style="width: 30rem;">
+                <br>
+                <img src="undraw_page_not_found_su7k.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title">Error Code: 404</h3>
+                    <p class="card-text">The HTTP 404, 404 Not Found, 404, Page Not Found, or Server Not Found error message is a Hypertext Transfer Protocol (HTTP) standard response code, in computer network communications, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested. The error may also be used when a server does not wish to disclose whether it has the requested information. The website hosting server will typically generate a "404 Not Found" web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web.</p>
+                    <div class="row text-center">
+                    <div class="col">
+                        <a href="#" class="btn btn-primary">Go Back</a>
+                    </div>
+                        <div class="col">
+                            <a href="mailto:admin@EmailServer.com" class="btn btn-success" >Contact Admin</a>
+                        </div>
+                    <div class="col">
+                        <a href="https://github.com/Yeehawlerz101/MysticCrm/issues" class="btn btn-danger">Bug Tracker</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+</div>
+<!-- Optional JavaScript -->
+<!-- Popper.js first, then Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php
